23a1b4235890d7e327e4abf9ed6c3397f434d61c
Amaya Perez <amayaperezcm@gmail.com>
2023-08-01 16:01:33 +0200

    Second Commit

--- topping-pizza/pizza-app.js
+++ topping-pizza/pizza-app.js
@@ -41,5 +41,68 @@ console.log(primerToppingNoMarron);
  console.log(toppingsNaranja);//Nos devuelve una lista de nodos.  
  console.log(toppingsNaranja.length);
 
+//Asignar estilos con JS
+const primerTopping = document.querySelector('.topping');
+console.log(primerTopping.style);//vemos en contenido css de ese elemento
 
+primerTopping.style.backgroundColor = 'blue';
+primerTopping.style.color = '#6dff00';
+primerTopping.style.textTransform = 'uppercase';
+
+//Acceder al texto 
+const listadeToppings = document.getElementById('lista-toppings');
+console.log(listadeToppings);
+console.log('--innerText--');
+console.log(listadeToppings.innerText);//para acceder al texto
+console.log('--textContent--');
+console.log(listadeToppings.textContent);//Deja la misma identacion que tenemos en nuestra estructura HTML
+console.log('--innerHTML--');
+console.log(listadeToppings.innerHTML);//Retorna la estructura HTML interna de ese elemento
+
+//Modificar texto
+const title = document.getElementById('titulo');
+console.log(title.innerText);
+title.innerText = 'Mis Toppings Favoritos'; //cambiamos el texto
+
+//Atributos -- vamos a ver como despues del listado de toppings vamos a agregar un enlace 
+const enlace = document.getElementsByTagName('a');
+console.log(enlace[0].getAttribute('href'));
+//console.log(enlace[0].removeAttribute('href'));//eliminar el atributo
+console.log(enlace[0].setAttribute('href', 'www.bing.com'));//para actualizar el atributo
+
+//CLASES
+const firstTopping = document.querySelector('.topping');
+console.log(firstTopping);
+console.log(firstTopping.classList);//cada uno de los elementos que seleccionamos del DOM, tiene una propiedad llamada classList, es decir lista de todas las clases que tiene ese elemento en el DOM//cada uno de los elementos que seleccionamos del DOM, tiene una propiedad llamada classList, es decir lista de todas las clases que tiene ese elemento en el DOM
+
+//Agregar una clase
+firstTopping.classList.add('mi-clase');
+console.log(firstTopping.classList);
+
+//Verificar si una clase existe
+secondTopping = document.querySelector('.fondo-naranja');
+console.log(secondTopping);
+console.log(secondTopping.classList.contains('topping'));//true
+console.log(secondTopping.classList.contains('fondo-negro'));//false
+
+//Eliminar una clase
+secondTopping.classList.remove('topping');
+
+//Crear, agregar y eliminar elementos del DOM
+//crear un elemento
+const listaDeToppings = document.getElementById('lista-toppings');//decimos donde queremos agregar ese nuevo elemento
+const toppingNuevo = document.createElement('li');//creamos nuevo elemento
+
+toppingNuevo.classList.add('topping', 'fondo-marron');//le agregamos un estilo mediante las clases
+toppingNuevo.innerText ='Queso Extra';
+
+//agregar un nuevo elemento
+listaDeToppings.append(toppingNuevo);
+
+//Eliminar un elemento
+toppingNuevo.remove();
+
+//Recorrer el DOM
+console.log(listaDeToppings.parentElement);//para saber el elemento padre
+console.log(listaDeToppings.parentNode);
 
